1a4f4891ed6127d9884f9d58d8b23fbfcdba648b
JOO97 <13295923180@163.com>
2022-04-09 21:07:23 +0800

    集成mapbox-gl

--- src/draw/handler/Draw.Circle.js
+++ src/draw/handler/Draw.Circle.js
@@ -5,42 +5,42 @@
  */
 L.Draw.Circle = L.Draw.SimpleShape.extend({
 	statics: {
-		TYPE: "circle",
+		TYPE: 'circle'
 	},
 
 	options: {
 		shapeOptions: {
 			stroke: true,
-			color: "#3388ff",
+			color: '#3388ff',
 			weight: 4,
 			opacity: 0.5,
 			fill: true,
 			fillColor: null, //same as color by default
 			fillOpacity: 0.2,
-			clickable: true,
+			clickable: true
 		},
 		showRadius: true,
 		metric: true, // Whether to use the metric measurement system or imperial
 		feet: true, // When not metric, use feet instead of yards for display
-		nautic: false, // When not metric, not feet use nautic mile for display
+		nautic: false // When not metric, not feet use nautic mile for display
 	},
 
 	// @method initialize(): void
 	initialize: function (map, options) {
 		// Save the type so super can fire, need to do this as cannot do this.TYPE :(
-		this.type = L.Draw.Circle.TYPE;
+		this.type = L.Draw.Circle.TYPE
 
-		this._initialLabelText = L.drawLocal.draw.handlers.circle.tooltip.start;
+		this._initialLabelText = L.drawLocal.draw.handlers.circle.tooltip.start
 
-		L.Draw.SimpleShape.prototype.initialize.call(this, map, options);
+		L.Draw.SimpleShape.prototype.initialize.call(this, map, options)
 	},
 
 	_drawShape: function (latlng) {
 		// Calculate the distance based on the version
 		if (L.GeometryUtil.isVersion07x()) {
-			var distance = this._startLatLng.distanceTo(latlng);
+			var distance = this._startLatLng.distanceTo(latlng)
 		} else {
-			var distance = this._map.distance(this._startLatLng, latlng);
+			var distance = this._map.distance(this._startLatLng, latlng)
 		}
 
 		if (!this._shape) {
@@ -48,10 +48,10 @@ L.Draw.Circle = L.Draw.SimpleShape.extend({
 				this._startLatLng,
 				distance,
 				this.options.shapeOptions
-			);
-			this._map.addLayer(this._shape);
+			)
+			this._map.addLayer(this._shape)
 		} else {
-			this._shape.setRadius(distance);
+			this._shape.setRadius(distance)
 		}
 	},
 
@@ -60,40 +60,40 @@ L.Draw.Circle = L.Draw.SimpleShape.extend({
 			this._startLatLng,
 			this._shape.getRadius(),
 			this.options.shapeOptions
-		);
-		console.log("circle", circle);
-		L.Draw.SimpleShape.prototype._fireCreatedEvent.call(this, circle);
+		)
+		console.log('set circle', circle)
+		L.Draw.SimpleShape.prototype._fireCreatedEvent.call(this, circle)
 	},
 
 	_onMouseMove: function (e) {
 		var latlng = e.latlng,
 			showRadius = this.options.showRadius,
 			useMetric = this.options.metric,
-			radius;
+			radius
 
-		this._tooltip.updatePosition(latlng);
+		this._tooltip.updatePosition(latlng)
 		if (this._isDrawing) {
-			this._drawShape(latlng);
+			this._drawShape(latlng)
 
 			// Get the new radius (rounded to 1 dp)
-			radius = this._shape.getRadius().toFixed(1);
+			radius = this._shape.getRadius().toFixed(1)
 
-			var subtext = "";
+			var subtext = ''
 			if (showRadius) {
 				subtext =
 					L.drawLocal.draw.handlers.circle.radius +
-					": " +
+					': ' +
 					L.GeometryUtil.readableDistance(
 						radius,
 						useMetric,
 						this.options.feet,
 						this.options.nautic
-					);
+					)
 			}
 			this._tooltip.updateContent({
 				text: this._endLabelText,
-				subtext: subtext,
-			});
+				subtext: subtext
+			})
 		}
-	},
-});
+	}
+})
